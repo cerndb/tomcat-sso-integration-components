512fd1f7331d1ce68e7a978860f50cd439bf5def
Jakub Granieczny <jakub.granieczny@cern.ch>
2020-05-20 09:03:57 +0000

    Resolve JEEDY-1830 "Ldap groups sso"

---
+++ cern-tomcat-authentication-kit/src/main/java/ch/cern/sso/tomcat/realms/JNDIUserRealm.java
@@ -0,0 +1,70 @@
+package ch.cern.sso.tomcat.realms;
+
+import org.apache.catalina.realm.JNDIRealm;
+import org.apache.catalina.realm.RealmBase;
+import org.apache.juli.logging.LogFactory;
+
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jgraniec
+ */
+public class JNDIUserRealm extends JNDIRealm {
+
+    private DirContext context;
+    public JNDIUserRealm(JNDIRealm jNDIRealm) {
+        this.setAuthentication(jNDIRealm.getAuthentication());
+        this.setConnectionName(jNDIRealm.getConnectionName());
+        this.setConnectionPassword(jNDIRealm.getConnectionPassword());
+        this.setConnectionURL(jNDIRealm.getConnectionURL());
+        this.setDerefAliases(jNDIRealm.getDerefAliases());
+        this.setProtocol(jNDIRealm.getProtocol());
+        this.setAdCompat(jNDIRealm.getAdCompat());
+        this.setReferrals(jNDIRealm.getReferrals());
+        this.setUserBase(jNDIRealm.getUserBase());
+        this.setUserSearch(jNDIRealm.getUserSearch());
+        this.setUserSubtree(jNDIRealm.getUserSubtree());
+        this.setUserPassword(jNDIRealm.getUserPassword());
+        this.setUserRoleAttribute(jNDIRealm.getUserRoleAttribute());
+        this.setUserPattern(jNDIRealm.getUserPattern());
+        this.setRoleBase(jNDIRealm.getRoleBase());
+        this.setUserRoleName(jNDIRealm.getUserRoleName());
+        this.setRoleName(jNDIRealm.getRoleName());
+        this.setRoleSearch(jNDIRealm.getRoleSearch());
+        this.setRoleSubtree(jNDIRealm.getRoleSubtree());
+        this.setRoleNested(jNDIRealm.getRoleNested());
+        this.setAlternateURL(jNDIRealm.getAlternateURL());
+        this.setCommonRole(jNDIRealm.getCommonRole());
+        this.setConnectionTimeout(jNDIRealm.getConnectionTimeout());
+        this.setReadTimeout(jNDIRealm.getReadTimeout());
+        this.setSizeLimit(jNDIRealm.getSizeLimit());
+        this.setSpnegoDelegationQop(jNDIRealm.getSpnegoDelegationQop());
+        this.setContainer(jNDIRealm.getContainer());
+        this.setUserRoleName(jNDIRealm.getRoleName());
+        try {
+            // Ensure that we have a directory context available
+            this.containerLog = LogFactory.getLog(RealmBase.class);
+            context = open();
+        } catch (NamingException e) {
+            // Log the problem for posterity
+            containerLog.error(sm.getString("jndiRealm.exception"), e);
+            if (context != null)
+                close(context);
+        }
+    }
+
+    public User getUserPublic(String username) throws NamingException {
+        return this.getUser(context, username, null, 0);
+    }
+    public List<String> getUserRoles(String username) throws NamingException {
+        return getUserRoles(getUserPublic(username));
+    }
+    private List<String> getUserRoles(User user) throws NamingException{
+        if(user == null)
+            return new ArrayList<>();
+        return getRoles(context,user);
+    }
+}
---
+++ cern-tomcat-authentication-kit/src/main/java/ch/cern/sso/tomcat/wrappers/SamlPrincipalWrapper.java
@@ -0,0 +1,73 @@
+package ch.cern.sso.tomcat.wrappers;
+
+import ch.cern.sso.tomcat.common.utils.Constants;
+import org.keycloak.adapters.saml.SamlPrincipal;
+import org.keycloak.common.util.MultivaluedHashMap;
+import org.keycloak.dom.saml.v2.assertion.AssertionType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jgraniec
+ */
+public class SamlPrincipalWrapper extends SamlPrincipal {
+    private List<String> roles;
+    public SamlPrincipalWrapper(SamlPrincipal samlPrincipal, List<String> roles){
+        this(samlPrincipal.getAssertion(),
+                samlPrincipal.getName(),
+                samlPrincipal.getSamlSubject(),
+                samlPrincipal.getNameIDFormat(),
+                getAttributes(samlPrincipal),
+                getFriendlyAttributes(samlPrincipal),
+                roles);
+    }
+
+    public SamlPrincipalWrapper(AssertionType assertion, String name, String samlSubject, String nameIDFormat, MultivaluedHashMap<String, String> attributes, MultivaluedHashMap<String, String> friendlyAttributes, List<String> roles) {
+        super(assertion, name, samlSubject, nameIDFormat, substituteAttributesRoles(attributes,roles), substituteAttributesRoles(friendlyAttributes,roles));
+        this.roles = roles;
+    }
+    public String getFriendlyAttribute(String friendlyName) {
+        if(friendlyName.equals(Constants.ROLES))
+            return (this.roles != null && this.roles.size() > 0) ? this.roles.get(0) : null;
+        return super.getFriendlyAttribute(friendlyName);
+    }
+    public String getAttribute(String name) {
+        if(name.equals(Constants.ROLES))
+            return (this.roles != null && this.roles.size() > 0) ? this.roles.get(0) : null;
+        return super.getAttribute(name);
+    }
+    public List<String> getFriendlyAttributes(String friendlyName) {
+        if(friendlyName.equals(Constants.ROLES))
+            return this.roles;
+        return super.getFriendlyAttributes(friendlyName);
+    }
+    public Map<String, List<String>> getAttributes() {
+        Map<String, List<String>> attributes = new HashMap<>(super.getAttributes());
+        attributes.put(Constants.ROLES,this.roles);
+        return attributes;
+    }
+    public List<String> getAttributes(String name) {
+        if(name.equals(Constants.ROLES))
+            return this.roles;
+        return super.getAttributes(name);
+    }
+    private static MultivaluedHashMap<String, String> substituteAttributesRoles(MultivaluedHashMap<String, String> attributes, List<String> roles){
+        MultivaluedHashMap<String, String> newAttributes = new MultivaluedHashMap<>(attributes);
+        newAttributes.remove(Constants.ROLES);
+        newAttributes.put(Constants.ROLES, roles);
+        return newAttributes;
+    }
+    public static MultivaluedHashMap<String, String> getAttributes(SamlPrincipal samlPrincipal){
+        MultivaluedHashMap<String, String> attributes = new MultivaluedHashMap<>();
+        attributes.putAll(samlPrincipal.getAttributes());
+        return attributes;
+    }
+    public static MultivaluedHashMap<String, String> getFriendlyAttributes(SamlPrincipal samlPrincipal){
+        MultivaluedHashMap<String, String> friendlyAttributes = new MultivaluedHashMap<>();
+        for(String friendlyName : samlPrincipal.getFriendlyNames())
+            friendlyAttributes.put(friendlyName, samlPrincipal.getFriendlyAttributes(friendlyName));
+        return friendlyAttributes;
+    }
+}
