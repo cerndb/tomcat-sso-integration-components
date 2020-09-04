1d8b63773888bb52efd1475d6837e08751725bb7
Jakub Granieczny <jakub.granieczny@cern.ch>
2020-09-04 16:20:15 +0200

    Add license header to all files

--- cern-tomcat-session-utils/src/main/java/ch/cern/sso/tomcat/common/utils/InitParamsUtils.java
+++ cern-tomcat-session-utils/src/main/java/ch/cern/sso/tomcat/common/utils/InitParamsUtils.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//        © Copyright 2020 CERN
+//        This software is distributed under the terms of the GNU General Public Licence
+//        version 3 (GPL Version 3), copied verbatim in the file "LICENSE". In applying
+//        this licence, CERN does not waive the privileges and immunities granted to it
+//        by virtue of its status as an Intergovernmental Organization or submit itself
+//        to any jurisdiction.
 package ch.cern.sso.tomcat.common.utils;
 
 import java.util.ResourceBundle;
